dbb3da078653a9d68c0ed33729ab782757e1a6aa
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-10-09 17:59:32 -0500

    [ ADD ] Propagation operator to add items to the list

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react'
 
 const Listas = () => {
-    const estadoInicial = [
-        {
-            id: 1, texto: 'tarea 1'
-        },
-        {
-            id: 2, texto: 'tarea 2'
-        },
-        {
-            id: 3, texto: 'tarea 3'
-        }
-    ]
-    const [lista, setLista] = useState(estadoInicial)
-    return (
-        <div>
-            <h2>Listas</h2>
-            {
-                lista.map((e) => <li key={e.id}>${e.texto}</li>)
-            }
-        </div>
-    )
+	const estadoInicial = [
+		{
+			id: 1, texto: 'tarea 1'
+		},
+		{
+			id: 2, texto: 'tarea 2'
+		},
+		{
+			id: 3, texto: 'tarea 3'
+		}
+	]
+	const [lista, setLista] = useState(estadoInicial)
+
+	const agregarElemento = () => {
+		setLista(
+			[
+				...lista, {
+					id: 4, texto: 'tarea 4'
+				}
+			]
+		)
+	}
+
+	return (
+		<div>
+			<h2>Listas</h2>
+			{
+				lista.map((e, index) => <li key={index}>${e.texto}</li>)
+			}
+			<button onClick={() => agregarElemento()}>Agregar</button>
+		</div>
+	)
 }
 
 export default Listas
