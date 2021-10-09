e48cd70ffae875ebb260d70f72e42919492ec7a8
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-10-08 20:37:28 -0500

    [ UPDATE ] Map and array of objects using useState

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 
 const Listas = () => {
-    const estadoInicial = [1,2,3,4,5]
+    const estadoInicial = [
+        {
+            id: 1, texto: 'tarea 1'
+        },
+        {
+            id: 2, texto: 'tarea 2'
+        },
+        {
+            id: 3, texto: 'tarea 3'
+        }
+    ]
     const [lista, setLista] = useState(estadoInicial)
     return (
         <div>
             <h2>Listas</h2>
             {
-                lista.map((e, index) => <li key={index}>${e}</li>)
+                lista.map((e) => <li key={e.id}>${e.texto}</li>)
             }
         </div>
     )
