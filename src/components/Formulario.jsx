ee59d143bb2a8bdc98b0c51c379b959bc4e15b4a
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-10-09 21:44:05 -0500

    [ ADD ] Ul tag to wrap li elements

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,22 +3,55 @@ import React from 'react'
 const Formulario = () => {
     const [fruta, setFruta] = React.useState('')
     const [descripcion, setDescripcion] = React.useState('')
+    const [listado, setlistado] = React.useState([])
+    
+    const guardarDatos = (e) => {
+        e.preventDefault()
+        if(!fruta.trim()){
+            console.log('Está vacío fruta')
+            return
+        }
+        if(!descripcion.trim()){
+            console.log('Está vacío descripcion')
+            return
+        }
+        setlistado([
+            ...listado, {
+                fruta,
+                descripcion
+            }
+        ])
+        console.log('procesando datos: '+  fruta + ' ' + descripcion)
+        e.target.reset()
+        setFruta('')
+        setDescripcion('')
+
+    }
     return (
         <div>
             <h2>Formulario</h2>
-            <form>
+            <form onSubmit={ guardarDatos}>
                 <input 
                     type="text"
                     placeholder='Ingrese la fruta'
                     className='form-control mb-2'
+                    onChange={ e => setFruta(e.target.value)}
                  />
                  <input 
                     type="text"
                     placeholder='Ingrese descripcion'
                     className='form-control mb-2'
+                    onChange={e => setDescripcion(e.target.value)}
                  />
-                 <button className="btn btn-primary btn-block">Agregar</button>
+                 <button className="btn btn-primary btn-block" type='submit'>Agregar</button>
+                 <h2>Listado ingresado</h2>
+                 <ul>
+                    {
+                        listado.map((e, index) => <li key={index}>{e.fruta} {e.descripcion}</li>)
+                    }
+                 </ul>
             </form>
+
         </div>
     )
 }
